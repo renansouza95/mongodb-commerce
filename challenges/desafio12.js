8ee6829e8faf97dca24bb5b6b5d014364d4bb056
renansouza95 <s.renan@outlook.com>
2022-06-27 19:57:56 -0300

    feat: desafio 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,11 @@
+db.produtos.updateMany(
+  { $and: [
+    { nome: { $ne: "McChicken" } },
+    { ingredientes: { $nin: ["ketchup"] } },
+  ] },
+  { $push: { 
+    ingredientes: "ketchup",
+   } },
+);
+
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
