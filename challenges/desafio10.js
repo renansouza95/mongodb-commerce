3b963044ba7df16a6e61c8af2ec5f98e4894b94c
renansouza95 <s.renan@outlook.com>
2022-06-27 19:22:37 -0300

    fix: utilizando  para pesquisar dentro de um array de objetos

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,5 +1,12 @@
-db.produtos.find({ $and: [
-    { "valoresNutricionais.tipo": "proteĩnas" },
-    { "valoresNutricionais.percentual": { $gte: 30, $lte: 40 } },
-  ] },
-  { _id: 0, nome: 1 });
+// db.produtos.find({
+//   $and: [
+//     { "valoresNutricionais.tipo": "proteínas" },
+//     { "valoresNutricionais.percentual": { $gte: 30, $lte: 40 } },
+//   ] },
+//   { _id: 0, nome: 1 });
+
+// https://stackoverflow.com/questions/14040562/how-to-search-in-array-of-object-in-mongodb
+
+db.produtos.find({
+  valoresNutricionais: { $elemMatch: { tipo: "proteínas", percentual: { $gte: 30, $lte: 40 } } },
+}, { _id: 0, nome: 1 });
--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,1 +1,12 @@
-db.produtos.find({ calorias: { $lt: 500 } }, { _id: 0, nome: 1 });
+// db.produtos.find({
+//   $and: [
+//     { "valoresNutricionais.tipo": "calorias" },
+//     { "valoresNutricionais.quantidade": { $lt: 500 } },
+//   ] },
+//   { _id: 0, nome: 1 });
+
+// https://stackoverflow.com/questions/14040562/how-to-search-in-array-of-object-in-mongodb
+
+db.produtos.find({
+  valoresNutricionais: { $elemMatch: { tipo: "calorias", quantidade: { $lt: 500 } } },
+}, { _id: 0, nome: 1 });
